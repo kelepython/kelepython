994d44878d2c4cf8088200d334380905d953bfc8
kelepython <517155992@qq.com>
2020-04-17 15:26:53 +0800

    change conf.py

--- source/conf.py
+++ source/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'可乐python说'
-copyright = u'2020, 可乐同学'
+copyright = u'2020可乐'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
